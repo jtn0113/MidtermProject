7551aa634b01a0fafdc977639d288cffd003c2c9
laurenackley <lackley127@gmail.com>
2022-12-21 16:43:17 -0600

    Create User entity, test Hibernate config

--- JPASip/src/test/java/com/skilldistillery/sip/entities/UserTest.java
+++ JPASip/src/test/java/com/skilldistillery/sip/entities/UserTest.java
@@ -1,6 +1,11 @@
 package com.skilldistillery.sip.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -9,26 +14,38 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
-
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private User user;
+	
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPASip");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user=null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_User_entity_mapping() {
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
+	
 	}
 
 }
